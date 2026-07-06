e91832670751ec33f4404da9725fefe45ab83d26
FSFR192 <fenandofloresrivera@gmail.com>
2026-07-06 17:11:17 -0500

    Resolver conflictos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.ferreteria_app"
-    compileSdk = 36
+    compileSdk {
+        version = release(36){
+            minorApiLevel = 1
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.ferreteria_app"
